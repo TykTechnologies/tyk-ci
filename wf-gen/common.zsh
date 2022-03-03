f1d1ad51911eb804a29b89f9f036c1573335fb4b
Asutosh <asutosh@tyk.io>
2022-03-03 10:42:46 +0530

    Update missing sync target files.

--- wf-gen/common.zsh
+++ wf-gen/common.zsh
@@ -2,6 +2,6 @@
 REPOS='tyk,tyk-analytics,tyk-pump,tyk-sink,tyk-identity-broker,raava'
 # Files to update for each repo, changes here need to be in meta.zsh
 TARGETS=(ci/goreleaser/goreleaser.yml ci/goreleaser/goreleaser-el7.yml ci/Dockerfile.std ci/Dockerfile.slim ci/aws/byol.pkr.hcl .github/workflows/release.yml .github/workflows/del-env.yml ci/terraform/outputs.tf .github/dependabot.yml ci/install/before_install.sh ci/install/post_install.sh ci/install/post_remove.sh ci/install/post_trans.sh)
-SYNC_AUTO_TARGETS=(ci .github/workflows/release.yml .github/dependabot.yml .github/workflows/del-env.yml)
+SYNC_AUTO_TARGETS=(ci .github/workflows/release.yml .github/dependabot.yml .github/workflows/del-env.yml .github/workflows/api-tests.yml .github/workflows/ui-tests.yml)
 # For each TARGETS, add SOURCE_SUFFIX to the basename to obtain the source file for that target
 SOURCE_SUFFIX=m4
