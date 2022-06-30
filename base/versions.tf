9fc65cc1491b7040346998ea214d276c3a8cbccb
Esteban Ricardo Mirizio <ermirizio@gmail.com>
2022-06-30 20:34:59 -0300

    fix prereq

--- base/versions.tf
+++ base/versions.tf
@@ -4,10 +4,6 @@ terraform {
       version = "> 3.0"
       source  = "hashicorp/aws"
     }
-    mongodbatlas = {
-      version = "> 1.3.0"
-      source = "mongodb/mongodbatlas"
-    }
   }
   required_version = ">= 0.14"
 }
