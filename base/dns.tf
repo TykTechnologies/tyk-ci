9048f4960fc328cebf82386330af3507a1ca4d9a
Razeen <Razeen.Abdal-Rahman@outlook.com>
2025-07-28 16:49:17 +0100

    updated cloudflair dns resource due to deprecation

--- base/dns.tf
+++ base/dns.tf
@@ -5,15 +5,15 @@ provider "cloudflare" {
   # account_id = "35b8134b47c7d01ee8198bb2b82a8dc5"
 }
 
-resource "cloudflare_record" "dev_tyk_tech" {
+resource "cloudflare_dns_record" "dev_tyk_tech" {
   for_each   = toset(aws_route53_zone.dev_tyk_tech.name_servers)
   depends_on = [aws_route53_zone.dev_tyk_tech]
 
   # This is the tyk.technology zone
   zone_id = "f3ee9e1c1e0e47f8ab60fae66d39aa8f"
-  name    = "dev"
+  name    = "dev.tyk.technology"
   type    = "NS"
-  value   = each.value
+  ttl     = 1
 }
 
 resource "aws_route53_zone" "dev_tyk_tech" {
