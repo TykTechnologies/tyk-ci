ba60eaa08d7562779f5d30b70181123361f37e2a
Razeen <Razeen.Abdal-Rahman@outlook.com>
2025-07-28 16:22:40 +0100

    updated ECS repo list to match live infrastrucutre

--- base/base.tf
+++ base/base.tf
@@ -7,11 +7,11 @@ locals {
   # name should match the tf workspace name
   name = "base-prod"
   # Repositories to create with per-repo access keys
-  repos = ["tyk", "tyk-analytics", "tyk-pump", "tyk-sink", "tyk-identity-broker", "portal", "tyk-sync"]
+  repos = ["tyk", "tyk-ee", "tyk-analytics", "tyk-pump", "tyk-pump-ee", "tyk-sink", "tyk-identity-broker", "portal", "tyk-sync", "tyk-plugin-compiler-ee"]
   # An additional repo that will be linked to the tyk user from repos above
   tyk_repos = ["tyk-plugin-compiler"]
   # repo list based on release cadence
-  low_cadence_repos  = ["tyk-pump", "tyk-sink", "tyk-identity-broker", "portal", "tyk-sync-internal", "tyk-plugin-compiler", "tyk-automated-tests"]
+  low_cadence_repos  = ["tyk-pump", "tyk-sink", "tyk-identity-broker", "portal", "tyk-sync", "tyk-plugin-compiler", "tyk-automated-tests"]
   high_cadence_repos = ["tyk", "tyk-analytics"]
 
   pr_policy1 = jsondecode(file("files/pr_policy.json"))
